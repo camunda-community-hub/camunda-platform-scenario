1fc562ad31d1879e517ca5e67bf65e8594eba47a
Martin Schimak <martin.schimak@plexiti.com>
2016-08-20 22:02:08 +0200

    test(userTask): add test

---
+++ src/test/java/org/camunda/bpm/scenario/test/AbstractTest.java
@@ -0,0 +1,29 @@
+package org.camunda.bpm.scenario.test;
+
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.scenario.Scenario;
+import org.junit.Before;
+import org.junit.Rule;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * @author Martin Schimak <martin.schimak@plexiti.com>
+ */
+public class AbstractTest {
+
+  @Rule
+  public ProcessEngineRule rule = new ProcessEngineRule();
+
+  @Mock
+  protected Scenario scenario;
+
+  @Mock
+  protected Scenario otherScenario;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+}
