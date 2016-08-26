ca3d3b2b7c04d6c316af25061d417dfeadebeef0
Martin Schimak <martin.schimak@plexiti.com>
2016-08-26 22:35:44 +0200

    fix(build): fix build for Camunda BPM 7.2.0 which fails due to a probable old bug with the @Deployment annotation

--- test-7.1/src/test/java/org/camunda/bpm/scenario/test/gateways/EventBasedGatewayTest.java
+++ test-7.1/src/test/java/org/camunda/bpm/scenario/test/gateways/EventBasedGatewayTest.java
@@ -1,22 +1,43 @@
 package org.camunda.bpm.scenario.test.gateways;
 
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.scenario.Scenario;
-import org.camunda.bpm.scenario.action.EventBasedGatewayAction;
-import org.camunda.bpm.scenario.delegate.EventBasedGatewayDelegate;
-import org.camunda.bpm.scenario.test.AbstractTest;
-import org.junit.Test;
-
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * @author Martin Schimak <martin.schimak@plexiti.com>
  */
 public class EventBasedGatewayTest extends org.camunda.bpm.scenario.test.waitstates.EventBasedGatewayTest {
 
-  // test class to find and execute this test under both directories
+  // test class to find and execute this test under both aspects 'gateways' and 'waitstates'
+  // methods are necessary for Camunda BPM 7.2.0 due to a bug with the @Deployment annotation
+
+  @Deployment(resources = {"org/camunda/bpm/scenario/test/waitstates/EventBasedGatewayTest.bpmn"})
+  public void testReceiveMessage() {
+    super.testReceiveMessage();
+  }
+
+  @Deployment(resources = {"org/camunda/bpm/scenario/test/waitstates/EventBasedGatewayTest.bpmn"})
+  public void testDoNothing() {
+    super.testDoNothing();
+  }
+
+  @Deployment(resources = {"org/camunda/bpm/scenario/test/waitstates/EventBasedGatewayTest.bpmn"})
+  public void testDoNotDealWithEventBasedGateway() {
+    super.testDoNotDealWithEventBasedGateway();
+  }
+
+  @Deployment(resources = {"org/camunda/bpm/scenario/test/waitstates/EventBasedGatewayTest.bpmn"})
+  public void testToBeforeEventBasedGateway() {
+    super.testToBeforeEventBasedGateway();
+  }
+
+  @Deployment(resources = {"org/camunda/bpm/scenario/test/waitstates/EventBasedGatewayTest.bpmn"})
+  public void testToAfterEventBasedGateway() {
+    super.testToAfterEventBasedGateway();
+  }
+
+  @Deployment(resources = {"org/camunda/bpm/scenario/test/waitstates/EventBasedGatewayTest.bpmn"})
+  public void testWhileOtherProcessInstanceIsRunning() {
+    super.testWhileOtherProcessInstanceIsRunning();
+  }
 
 }
