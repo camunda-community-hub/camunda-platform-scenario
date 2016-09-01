0fe005979a13694a83177647f8d210083f01f7e9
Martin Schimak <martin.schimak@plexiti.com>
2016-09-01 12:27:07 +0200

    refactor(api): further improve upon naming and packaging details

--- src/test/java/org/camunda/bpm/scenario/examples/insuranceapplication/InsuranceApplicationProcessTest.java
+++ src/test/java/org/camunda/bpm/scenario/examples/insuranceapplication/InsuranceApplicationProcessTest.java
@@ -3,9 +3,9 @@ package org.camunda.bpm.scenario.examples.insuranceapplication;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.scenario.ExecutedScenario;
 import org.camunda.bpm.scenario.ProcessScenario;
 import org.camunda.bpm.scenario.Scenario;
-import org.camunda.bpm.scenario.runner.ScenarioRun;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -102,12 +102,12 @@ public class InsuranceApplicationProcessTest {
 
     variables.put("riskAssessment", "green");
 
-    ScenarioRun run = Scenario.run(insuranceApplication)
+    ExecutedScenario scenario = Scenario.run(insuranceApplication)
         .startByKey("InsuranceApplication", variables) // either just start process by key ...
         .fromBefore("EndEventApplicationAccepted")
         .execute();
 
-    assertThat(run.getProcessInstance()).variables().containsEntry("riskAssessment", "green");
+    assertThat(scenario.getProcessInstance()).variables().containsEntry("riskAssessment", "green");
     verify(insuranceApplication, never()).hasStarted("SubProcessManualCheck");
     verify(insuranceApplication).hasFinished("EndEventApplicationAccepted");
 
@@ -121,13 +121,13 @@ public class InsuranceApplicationProcessTest {
       .putValue("carManufacturer", "Porsche")
       .putValue("carType", "911");
 
-    ScenarioRun run = Scenario.run(insuranceApplication)
+    ExecutedScenario scenario = Scenario.run(insuranceApplication)
       .startBy(() -> { // ... or define your own starter function
         return rule.getRuntimeService().startProcessInstanceByKey("InsuranceApplication", variables);
       })
       .execute();
 
-    assertThat(run.getProcessInstance()).variables().containsEntry("riskAssessment", "yellow");
+    assertThat(scenario.getProcessInstance()).variables().containsEntry("riskAssessment", "yellow");
     verify(insuranceApplication).hasCompleted("SubProcessManualCheck");
     verify(insuranceApplication).hasFinished("EndEventApplicationAccepted");
 
@@ -141,11 +141,11 @@ public class InsuranceApplicationProcessTest {
       .putValue("carManufacturer", "Porsche")
       .putValue("carType", "911");
 
-    ScenarioRun run = Scenario.run(insuranceApplication)
+    ExecutedScenario scenario = Scenario.run(insuranceApplication)
         .startByKey("InsuranceApplication", variables)
         .execute();
 
-    assertThat(run.getProcessInstance()).variables().containsEntry("riskAssessment", "red");
+    assertThat(scenario.getProcessInstance()).variables().containsEntry("riskAssessment", "red");
 
     verify(insuranceApplication, never()).hasStarted("SubProcessManualCheck");
     verify(insuranceApplication).hasFinished("EndEventApplicationRejected");
@@ -160,11 +160,11 @@ public class InsuranceApplicationProcessTest {
       .putValue("carManufacturer", "Porsche")
       .putValue("carType", "911");
 
-    ScenarioRun run = Scenario.run(insuranceApplication)
+    ExecutedScenario scenario = Scenario.run(insuranceApplication)
         .startByKey("InsuranceApplication", variables)
         .execute();
 
-    assertThat(run.getProcessInstance()).variables()
+    assertThat(scenario.getProcessInstance()).variables()
       .containsEntry("riskAssessment", "yellow")
       .containsEntry("approved", true);
 
@@ -185,11 +185,11 @@ public class InsuranceApplicationProcessTest {
       task.complete(withVariables("approved", false));
     });
 
-    ScenarioRun run = Scenario.run(insuranceApplication)
+    ExecutedScenario scenario = Scenario.run(insuranceApplication)
         .startByKey("InsuranceApplication", variables)
         .execute();
 
-    assertThat(run.getProcessInstance()).variables()
+    assertThat(scenario.getProcessInstance()).variables()
       .containsEntry("riskAssessment", "yellow")
       .containsEntry("approved", false);
 
