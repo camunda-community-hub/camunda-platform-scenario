d363320bcf124468cbd78d697772db2c05bbc015
Martin Schimak ☕ <martinschimak@users.noreply.github.com>
2021-05-02 11:48:16 +0200

    Add handling bpmn error functionality to user task delegates (#46)
    * Add handling bpmn error functionality to user task delegates
    * Closes #45

---
+++ test/test-7.12/src/test/java/org/camunda/bpm/scenario/test/errors/UserTaskBoundaryErrorEventTest.java
@@ -0,0 +1,52 @@
+package org.camunda.bpm.scenario.test.errors;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.scenario.Scenario;
+import org.camunda.bpm.scenario.act.UserTaskAction;
+import org.camunda.bpm.scenario.delegate.TaskDelegate;
+import org.camunda.bpm.scenario.test.AbstractTest;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author <a href="martin.schimak@plexiti.com">Martin Schimak</a>
+ */
+@Deployment(resources = {"org/camunda/bpm/scenario/test/errors/BoundaryErrorEventTest.bpmn"})
+public class UserTaskBoundaryErrorEventTest extends AbstractTest {
+
+  @Test
+  public void testCompleteTask() {
+
+    when(scenario.waitsAtUserTask("UserTask")).thenReturn(new UserTaskAction() {
+      @Override
+      public void execute(TaskDelegate task) {
+        task.complete();
+      }
+    });
+
+    Scenario.run(scenario).startByKey("BoundaryErrorEventTest").execute();
+
+    verify(scenario, times(1)).hasFinished("EndEventCompleted");
+    verify(scenario, never()).hasFinished("EndEventError");
+
+  }
+
+  @Test
+  public void testHandleBpmnError() {
+
+    when(scenario.waitsAtUserTask("UserTask")).thenReturn(new UserTaskAction() {
+      @Override
+      public void execute(TaskDelegate task) {
+        task.handleBpmnError("errorCode");
+      }
+    });
+
+    Scenario.run(scenario).startByKey("BoundaryErrorEventTest").execute();
+
+    verify(scenario, never()).hasFinished("EndEventCompleted");
+    verify(scenario, times(1)).hasFinished("EndEventError");
+
+  }
+
+}
