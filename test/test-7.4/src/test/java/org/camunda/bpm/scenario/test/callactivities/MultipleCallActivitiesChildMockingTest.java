5643ac736afeda66bda8273da766bd92da11a731
Martin Schimak <martin.schimak@plexiti.com>
2021-05-04 20:44:01 +0200

    Fix registering multiple call activities - closes #59

---
+++ test/test-7.4/src/test/java/org/camunda/bpm/scenario/test/callactivities/MultipleCallActivitiesChildMockingTest.java
@@ -0,0 +1,55 @@
+package org.camunda.bpm.scenario.test.callactivities;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.scenario.Scenario;
+import org.camunda.bpm.scenario.act.MockedCallActivityAction;
+import org.camunda.bpm.scenario.delegate.ExternalTaskDelegate;
+import org.camunda.bpm.scenario.test.AbstractTest;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author <a href="martin.schimak@plexiti.com">Martin Schimak</a>
+ */
+public class MultipleCallActivitiesChildMockingTest extends AbstractTest {
+
+  @Test
+  @Deployment(resources = { "org/camunda/bpm/scenario/test/callactivities/MultipleCallActivitiesTest.bpmn" })
+  public void testCompleteCallActivities() {
+
+    when(scenario.waitsAtMockedCallActivity("CallActivity1")).thenReturn(new MockedCallActivityAction() {
+      @Override
+      public void execute(ExternalTaskDelegate callActivity) {
+        callActivity.complete();
+      }
+    });
+
+    when(scenario.waitsAtMockedCallActivity("CallActivity2")).thenReturn(new MockedCallActivityAction() {
+      @Override
+      public void execute(ExternalTaskDelegate callActivity) {
+        callActivity.complete();
+      }
+    });
+
+    when(scenario.waitsAtMockedCallActivity("CallActivity3")).thenReturn(new MockedCallActivityAction() {
+      @Override
+      public void execute(ExternalTaskDelegate callActivity) {
+        callActivity.complete();
+      }
+    });
+
+    Scenario.run(scenario)
+        .withMockedProcess("Child1")
+        .withMockedProcess("Child2")
+        .startByKey("MultipleCallActivitiesTest")
+        .execute();
+
+    verify(scenario, times(1)).hasFinished("CallActivity1");
+    verify(scenario, times(1)).hasFinished("CallActivity2");
+    verify(scenario, times(1)).hasFinished("CallActivity3");
+    verify(scenario, times(1)).hasFinished("EndEvent");
+
+  }
+
+}
